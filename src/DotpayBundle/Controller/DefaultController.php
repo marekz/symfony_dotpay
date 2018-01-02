dd3e879752d2783737ea9b213bbb7632243be2f8
Marek Zdybel <marek@motta.com.pl>
2018-01-02 23:59:45 +0100

    Wyprowadzenie parametrow z parameters.yaml

--- src/DotpayBundle/Controller/DefaultController.php
+++ src/DotpayBundle/Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace DotpayBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\DependencyInjection\Container;
 
 class DefaultController extends Controller
 {
@@ -12,6 +13,11 @@ class DefaultController extends Controller
      */
     public function indexAction()
     {
+	$dotpay_address	    = $this->container->getParameter('dotpay_address');
+	$dotpay_id	    = $this->container->getParameter('dotpay_id');
+	$dotpay_pin	    = $this->container->getParameter('dotpay_pin');
+	var_dump($dotpay_pin);
+	
         return $this->render('@Dotpay/Default/index.html.twig');
     }
 }
